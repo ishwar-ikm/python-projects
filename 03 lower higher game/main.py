5acd9f56bc31dd57e0d4fa85f6d7d3b825a781af
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-06-19 11:59:30 +0530

    Add files via upload

---
+++ 03 lower higher game/main.py
@@ -0,0 +1,36 @@
+from art import *
+from game_data import *
+import random
+
+score = 0
+person = []
+person.append(random.choice(data))
+end = False
+while not end:
+    temp = random.choice(data)
+    if temp in person:
+        # print("running again")
+        continue
+    person.append(temp)
+    # print(f"\n{person[len(person)-2]['follower_count']}\n{person[len(person)-1]['follower_count']}\n")
+
+    print(f"Compare A: {person[len(person)-2]['name']}, a {person[len(person)-2]['description']}, from {person[len(person)-2]['country']}")
+    print(vs)
+    print(f"Against B: {person[len(person) - 1]['name']}, a {person[len(person) - 1]['description']}, from {person[len(person) - 1]['country']}")
+    choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if choice == 'a':
+        human = len(person)-2
+        other = len(person)-1
+    else:
+        human = len(person)-1
+        other = len(person)-2
+
+    if person[human]['follower_count'] >= person[other]['follower_count']:
+        score += 1
+        print(f"\nCurrent score {score}")
+    else:
+        end = True
+        print(f"Your final score is {score}")
+    if score == 50:
+        print(f"You scored maximum points congratulations!")
+        end = True
