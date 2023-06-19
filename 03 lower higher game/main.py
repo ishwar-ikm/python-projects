f9332db521e326d33969ce025e4cdef699789601
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-06-19 12:06:45 +0530

    Update main.py

--- 03 lower higher game/main.py
+++ 03 lower higher game/main.py
@@ -18,7 +18,13 @@ while not end:
     print(f"Compare A: {person[len(person)-2]['name']}, a {person[len(person)-2]['description']}, from {person[len(person)-2]['country']}")
     print(vs)
     print(f"Against B: {person[len(person) - 1]['name']}, a {person[len(person) - 1]['description']}, from {person[len(person) - 1]['country']}")
+    
+    while True:
     choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if choice == 'a' or choice == 'b':
+        break
+    print("Wrong input Try Again!")
+    
     if choice == 'a':
         human = len(person)-2
         other = len(person)-1
