a3ac7c1b2bacf38d565ef6af52776e5202a43aea
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-06-25 22:25:06 +0530

    Update food.py

--- 09 Snake Game/food.py
+++ 09 Snake Game/food.py
@@ -2,7 +2,10 @@ import turtle
 import random
 
 
+# Create a food class
 class Food(turtle.Turtle):
+
+    # Define the constructor for the appearance of the food
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -12,8 +15,8 @@ class Food(turtle.Turtle):
         self.shapesize(0.6)
         self.set_position()
 
+    # Method to randomly set the position of the food
     def set_position(self):
         x = random.randint(-280, 280)
         y = random.randint(-280, 255)
         self.goto(x, y)
-
