da7a01d1bdfe2e66e4e9ac852abc7bf0e6a3e06f
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-06-25 22:20:48 +0530

    Update snake.py

--- 09 Snake Game/snake.py
+++ 09 Snake Game/snake.py
@@ -1,17 +1,19 @@
 import turtle
 
-
+# Creating a Snake class
 class Snake:
-    body = []
+    body = [] # Attribute that contains turtle objects which makes the body of the snake
 
+    # Defining a constructor to make the initial body of the snake
     def __init__(self):
         x = 0
         pos = (x, 0)
         for i in range(3):
             self.add_tail(pos)
             x -= 20
-        self.head = self.body[0]
+        self.head = self.body[0] # Define the head attribute
 
+    # The method to add tail
     def add_tail(self, position):
         tail = turtle.Turtle(shape="square")
         tail.color("white")
@@ -19,14 +21,17 @@ class Snake:
         tail.goto(position)
         self.body.append(tail)
 
+    # Method to extend the tail
     def extend(self):
         self.add_tail(self.body[-1].position())
 
+    # Method to move the snake by following the head
     def move(self):
         for i in range(len(self.body) - 1, 0, -1):
             self.body[i].goto(self.body[i - 1].pos())
         self.head.forward(20)
 
+    # Creating methods for the movement of head according to the input
     def up(self):
         if self.head.heading() != 270:
             self.head.setheading(90)
