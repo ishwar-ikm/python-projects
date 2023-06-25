0e4a6798e0c6c225a46579752cc86fd1c088d7a1
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-06-25 22:27:37 +0530

    Update scoreboard.py

--- 09 Snake Game/scoreboard.py
+++ 09 Snake Game/scoreboard.py
@@ -1,7 +1,9 @@
 import turtle
 
-
+# Create a class score to update and keep track for the score
 class Score(turtle.Turtle):
+
+    # Define the constructor for the appearance of score board and its position
     def __init__(self):
         super().__init__()
         self.score = 0
@@ -11,14 +13,17 @@ class Score(turtle.Turtle):
         self.goto(0, 260)
         self.display()
 
+    # Method to display the score
     def display(self):
         self.write(f"Score = {self.score}", align="center", font=("Arial", 24, "normal"))
 
+    # Method to update the score
     def inc_score(self):
         self.score += 1
         self.clear()
         self.display()
 
+    # Method to display game over sequence
     def game_over(self):
         self.goto(0, 0)
         self.write("Game Over", align="center", font=("Arial", 20, "normal"))
