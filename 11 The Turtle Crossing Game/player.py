c3bd54c9fea666db5c2e2d0d1a70c3da9185745e
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-06-29 12:22:02 +0530

    Update player.py

--- 11 The Turtle Crossing Game/player.py
+++ 11 The Turtle Crossing Game/player.py
@@ -1,7 +1,9 @@
 import turtle
 
 
+# In the Player class, which inherits Turtle class, the player's properties and behavior are defined
 class Player(turtle.Turtle):
+    # Constructor to define the appearance and properties of the player 
     def __init__(self):
         super().__init__()
         self.shape("turtle")
@@ -10,18 +12,22 @@ class Player(turtle.Turtle):
         self.goto(0, -270)
         self.setheading(90)
 
+    # Method to move the turtle up
     def move_up(self):
         self.forward(20)
 
+    # Method to move the turtle left
     def move_left(self):
         self.lt(90)
         self.forward(20)
         self.rt(90)
 
+    # Method to move the turtle right
     def move_right(self):
         self.rt(90)
         self.forward(20)
         self.lt(90)
 
+    # Method to reset the turtle position back to the begining
     def restart(self):
         self.goto(0, -270)
