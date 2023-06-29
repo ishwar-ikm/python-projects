7f9905bb9ae46a386d7146aef4abe96bda14feb7
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-06-29 12:28:49 +0530

    Update scorecard.py

--- 11 The Turtle Crossing Game/scorecard.py
+++ 11 The Turtle Crossing Game/scorecard.py
@@ -1,18 +1,22 @@
 import turtle
 
 
+# In the ScoreBoards class, which inherits Turtle class, the player's score is tracked and displayed
 class ScoreBoards(turtle.Turtle):
+    # Constructor to set the color of the text and other attributes
     def __init__(self):
         super().__init__()
         self.up()
         self.hideturtle()
         self.color("black")
-        self.level = 0
+        self.level = 0  # level attribute to keep track of the level
 
+    # Method to show the game over text and the score player scored
     def game_over(self):
         self.goto(0, 0)
         self.write(f"    Game Over\nYour score was {self.level}", align='center', font=("Arial", 20, 'normal'))
 
+    # Method increment the level attribute and display the level on the screen
     def level_inc(self):
         self.level += 1
         self.clear()
