6755555c2d050c392ef676bf066c64fe93002c8e
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-07-12 20:11:35 +0530

    Update question_model.py

--- 18 GUI Quiz App/question_model.py
+++ 18 GUI Quiz App/question_model.py
@@ -1,4 +1,10 @@
 class Question:
     def __init__(self, text, ans):
+        """
+        Initializes a Question object with the provided text and answer.
+
+        :param text: The text of the question.
+        :param ans: The correct answer to the question.
+        """
         self.text = text
         self.ans = ans
