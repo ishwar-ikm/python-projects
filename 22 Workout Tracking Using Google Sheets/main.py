f666cddd34c3f8a0b159676ebe1f872bb326f075
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-07-16 22:15:18 +0530

    Update main.py

--- 22 Workout Tracking Using Google Sheets/main.py
+++ 22 Workout Tracking Using Google Sheets/main.py
@@ -3,8 +3,8 @@ import os
 import requests
 
 # Constants
-API_ID = os.environ["NT_APP_ID"]
-API_KEY = os.environ["NT_API_KEY"]
+API_ID = os.environ["NT_APP_ID"]  # Nutritionix API ID
+API_KEY = os.environ["NT_API_KEY"]  # Nutritionix API Key
 GENDER = "male"
 WEIGHT = 82
 HEIGHT = 177.8
@@ -12,15 +12,15 @@ AGE = 21
 
 
 # Nutritionix Setup and getting the exercise details
-endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheety_endpoint = os.environ["SHEET_ENDPOINT"]
+endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"  # API endpoint for exercise tracking
+sheety_endpoint = os.environ["SHEET_ENDPOINT"]  # Sheety API endpoint for Google Sheets
 
 header = {
     'x-app-id': API_ID,
     'x-app-key': API_KEY,
 }
 
-text = input("What you did today: ")
+text = input("What you did today: ")  # Prompt the user for exercise details
 
 parameters = {
     "query": text,
@@ -30,13 +30,14 @@ parameters = {
     "age": AGE
 }
 
-response = requests.post(endpoint, json=parameters, headers=header)
+response = requests.post(endpoint, json=parameters, headers=header)  # Send request to Nutritionix API
 
 
-# Using sheety to update the google sheets
-today = datetime.datetime.now().strftime("%d-%m-%Y")
-time = datetime.datetime.now().strftime("%X")
+# Using Sheety to update the Google Sheets
+today = datetime.datetime.now().strftime("%d-%m-%Y")  # Current date
+time = datetime.datetime.now().strftime("%X")  # Current time
 
+# Iterate over each exercise in the response
 for exercise in response.json()['exercises']:
     sheet_parameter = {
         'workout': {
@@ -48,4 +49,5 @@ for exercise in response.json()['exercises']:
         }
     }
 
+    # Update the Google Sheets spreadsheet using Sheety API
     print(requests.post(sheety_endpoint, json=sheet_parameter).text)
