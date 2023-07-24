deb5c4e04ce60a2f62314f2c95a8d41c7ee84a81
Ishwar <109938051+ishwar-ikm@users.noreply.github.com>
2023-07-24 22:38:58 +0530

    Update main.py

--- 04 coffee machine/main.py
+++ 04 coffee machine/main.py
@@ -1,3 +1,4 @@
+# Define the menu of available beverages with their ingredients and cost
 menu = {
     "espresso": {
         "ingredients": {
@@ -24,6 +25,7 @@ menu = {
     }
 }
 
+# Define the available amount of ingredients and profit of the machine
 amount = {
     'water': 800,
     'milk': 550,
@@ -31,36 +33,36 @@ amount = {
     'profit': 0,
 }
 
-
+# Function to display the current status of resources and profit
 def report():
     print(f"Water: {amount['water']}ml")
     print(f"Milk: {amount['milk']}ml")
     print(f"Coffee: {amount['coffee']}g")
     print(f"Profit: ${amount['profit']}")
 
-
+# Function to receive payment and return the total amount
 def payment():
-    quarters = int(input("How many quarters: "))*0.25
+    quarters = int(input("How many quarters: ")) * 0.25
     dimes = int(input("How many dimes: ")) * 0.1
-    nickles = int(input("How many nickles: ")) * 0.05
+    nickels = int(input("How many nickels: ")) * 0.05
     pennies = int(input("How many pennies: ")) * 0.01
-    return quarters+dimes+nickles+pennies
+    return quarters + dimes + nickels + pennies
 
-
+# Function to check if the payment is sufficient for the selected beverage
 def is_sufficient_amount(payment_amount, order):
     if payment_amount >= menu[order]['cost']:
         return True
     else:
         return False
 
-
+# Function to update the resources and profit after successful transaction
 def make_transaction(payment_amount, order):
     amount['profit'] += menu[order]['cost']
     for item in menu[order]['ingredients']:
         amount[item] -= menu[order]['ingredients'][item]
     return payment_amount - menu[order]['cost']
 
-
+# Function to check if there are enough resources to make the selected beverage
 def is_enough_resources(order):
     ingredients = menu[order]['ingredients']
     for item in ingredients:
@@ -69,7 +71,7 @@ def is_enough_resources(order):
             return False
     return True
 
-
+# Function to control the coffee machine and handle user choices
 def machine():
     while True:
         choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
@@ -94,6 +96,7 @@ def machine():
                 print("Sorry not enough balance added! Refunding amount...")
 
 
+# Main loop to keep the coffee machine running
 while True:
     choice = machine()
     if choice == "off":
